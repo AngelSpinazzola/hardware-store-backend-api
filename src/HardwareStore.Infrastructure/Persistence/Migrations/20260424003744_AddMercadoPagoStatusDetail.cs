032f857ba5fc6c821ce502cb084e7e4cd1dab1ba
Angel Spinazzola <angel_spinazzola@outlook.es>
2026-04-23 21:49:16 -0300

    chore(db): agregar migración AddMercadoPagoStatusDetail

---
+++ src/HardwareStore.Infrastructure/Persistence/Migrations/20260424003744_AddMercadoPagoStatusDetail.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HardwareStore.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMercadoPagoStatusDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MercadoPagoStatusDetail",
+                table: "Orders",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MercadoPagoStatusDetail",
+                table: "Orders");
+        }
+    }
+}
